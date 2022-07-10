c95a5b4342da00193e5699a4d4b4cca721a21975
BalaGanesh I <balaig7@gmail.com>
2022-07-10 14:11:00 +0530

    files modified

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -67,51 +67,57 @@ function showHospitals(district_id, date) {
             $('html,body').animate({
                 scrollTop: $(".hospitals").offset().top
             }, '1000');
+            $(this).fadeIn("slow");
+
             var blocks = [...new Map(data.sessions.map(item => [item['block_name'], item])).values()];
             if (blocks.length > 2) {
-                hospitals += '<div class="d-flex blocks">'
-                hospitals += '<div class="col">'
+                hospitals += '<div class="d-flex blocks justify-content-md-center">'
+                hospitals += '<div>'
                 hospitals += '<button type="button" class="btn mx-1 btn-outline-primary categories rounded-pill" data-filter="all">All</button></div>'
                 $.each(blocks, function(ind, value) {
-                    hospitals += '<div class="col ">'
+                    hospitals += '<div>'
 
                     hospitals += '<button class="btn mx-1 btn-outline-primary categories rounded-pill" data-filter="' + value.block_name.replace(/ /g, "-") + '">' + value.block_name + '</div>'
                 })
                 hospitals += '</div>'
             }
-            hospitals += '<h5>Hospitals available on - ' + date + '</h5>'
+            hospitals += '<h5>Hospitals available on (' + date + ')</h5>'
 
             hospitals += '<div class="row">'
-            $.each(data.sessions, function(index, value) {
-                hospitals += '<div class="col-lg-3 details col-sm-12 p-3 ' + value.block_name.replace(/ /g, "-") + ' ">'
-                hospitals += '<div class="card border-0 " onclick="viewDetails();">'
-                hospitals += '<div class="card-header bg-primary text-white text-wrap" data-bs-target="#collapse-' + value.center_id + '">'
-                hospitals += '<i class="fa fa-plus-square fa-2x"></i><span class="h-name mx-2">' +
-                    value.name + '</span><i class="fa fa-chevron-down mt-2" ></i></div>'
-                hospitals += '<div class="card-body" id="collapse-' + value.center_id + '">'
-                hospitals += '<p><i class="fas fa-map-marker mx-1"></i>' + value.address + '</p>'
-                hospitals += '<p><i class="fas fa-rupee-sign mx-1"></i>' + value.fee + '</p>'
-                hospitals += '<p><i class="fas fa-syringe mx-1"></i>' + value.vaccine + ' (<b>' + value.available_capacity + '</b>) </p>'
-                hospitals += '<div class="mx-4 mb-1">Dose 1 - <b>' + value.available_capacity_dose1 + '</b></div>'
-                hospitals += '<div class="mx-4 mb-3">Dose 2 - <b>' + value.available_capacity_dose2 + '</b></div>'
-                if (value.max_age_limit == undefined) {
-                    value.max_age_limit = ' & above'
-                } else {
-                    value.max_age_limit = ' - ' + value.max_age_limit
-                }
+            if(data.sessions.length>0){
+                $.each(data.sessions, function(index, value) {
+                    hospitals += '<div class="col-lg-3 details col-sm-12 p-3 ' + value.block_name.replace(/ /g, "-") + ' ">'
+                    hospitals += '<div class="card border-0 ">'
+                    hospitals += '<div class="card-header bg-primary text-white text-wrap" data-bs-target="#collapse-' + value.center_id + '">'
+                    hospitals += '<i class="fa fa-plus-square fa-2x"></i><span class="h-name mx-2">' +
+                        value.name + '</span><i class="fa fa-chevron-down mt-2" ></i></div>'
+                    hospitals += '<div class="card-body" id="collapse-' + value.center_id + '">'
+                    hospitals += '<p><i class="fas fa-map-marker mx-1"></i>' + value.address + '</p>'
+                    hospitals += '<p><i class="fas fa-rupee-sign mx-1"></i>' + value.fee + '</p>'
+                    hospitals += '<p><i class="fas fa-syringe mx-1"></i>' + value.vaccine + ' (<b>' + value.available_capacity + '</b>) </p>'
+                    hospitals += '<div class="mx-4 mb-1">Dose 1 - <b>' + value.available_capacity_dose1 + '</b></div>'
+                    hospitals += '<div class="mx-4 mb-3">Dose 2 - <b>' + value.available_capacity_dose2 + '</b></div>'
+                    if (value.max_age_limit == undefined) {
+                        value.max_age_limit = ' & above'
+                    } else {
+                        value.max_age_limit = ' - ' + value.max_age_limit
+                    }
 
-                hospitals += '<p>Age Limit - <span ><b>' + value.min_age_limit + value.max_age_limit + '</b></span></p>'
-                hospitals += '<p><i class="far fa-clock mx-1"></i>Timings</p>'
-                $.each(value.slots, function(ind, availableslots) {
-                    hospitals += '<p class="rounded border slots border-primary text-center">' + availableslots + '</p>'
+                    hospitals += '<p>Age Limit - <span ><b>' + value.min_age_limit + value.max_age_limit + '</b></span></p>'
+                    hospitals += '<p><i class="far fa-clock mx-1"></i>Timings</p>'
+                    $.each(value.slots, function(ind, availableslots) {
+                        hospitals += '<p class="rounded border slots border-primary text-center">' + availableslots.time + '</p>'
+                    })
+
+
+                    hospitals += '</div>'
+                    hospitals += '</div>'
+                    hospitals += '</div>'
+
                 })
-
-
-                hospitals += '</div>'
-                hospitals += '</div>'
-                hospitals += '</div>'
-
-            })
+            }else{
+                hospitals +='<h4 class="text-center" style="margin-top:10%">Sorry no vaccination providers available in this date</h4>'
+            }
             hospitals += '</div>'
             $('.hospitals').html(hospitals)
         }
@@ -129,9 +135,12 @@ $(document).on('click', '.categories', function() {
         var category = $(this).data('filter');
         $('.details').each(function() {
             if (category == 'all') {
+                $(this).fadeIn("slow");
                 $(this).show()
             } else {
+                $(this).fadeIn("slow");
                 $(this).hide().filter('.' + category).show();
+
             }
         })
     })
